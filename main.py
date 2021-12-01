fdcb074dc56891a80faeef8d8d4d7429008f4f5e
luigiloiola <60331397+luigiloiola@users.noreply.github.com>
2021-12-01 00:02:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ raze = ('raze',1142,1004)
 agentes = [chamber, cypher, jett, kayo, ekko, reyna, sage, sova, viper, raze]
 print(agentes)
 
-
-
 def pick_valorant(agente=jett):
     pos = imagesearch_loop(r'C:\Users\Desktop\OneDrive\Imagens\Capturas de tela\cypher.png',1)
     if pos:
@@ -36,51 +34,3 @@ for i in agentes:
 
 pick_valorant(boneco)
 
-
-
-
-
-
-#
-# try:
-#     while True:
-#         x, y = pyautogui.position()
-#         position = f'x:{x}  //  y:{y}'
-#         print(position)
-#         sleep(5)
-# except:
-#     pass
-
-
-
-
-#
-# def is_loop():
-#     pos = imagesearch_loop(r'C:\Users\Desktop\OneDrive\Imagens\Capturas de tela\encontrar partrida.png',1)
-#     if pos:
-#         pyautogui.click(x=pos[0], y=pos[1])
-
-
-    # pos = imagesearch_loop(r'C:\Users\Desktop\OneDrive\Imagens\Capturas de tela\role.png', 1)
-    # if pos:
-    #     print(pos)
-    #     pyautogui.click(x=pos[0], y=pos[1])
-    #     pyautogui.move(0, -94)
-    #     pyautogui.click()
-    # pos = imagesearch_loop(r'C:\Users\Desktop\OneDrive\Imagens\Capturas de tela\role.png', 1)
-    # if pos:
-    #     pyautogui.click(x=pos[0], y=pos[1])
-    #     pyautogui.move(-98, 0)
-    #     pyautogui.click()
-    #
-
-        # pos2 = imagesearch_loop(r'C:\Users\Desktop\OneDrive\Imagens\Capturas de tela\role.png', 1)
-# is_loop()
-
-
-# # x:404  //  y:196
-
-
-# x:802  //  y:639
-# x:802  //  y:545
-# x:704  //  y:631
